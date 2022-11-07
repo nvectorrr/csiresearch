f3e6bf94702969357872d96a76286140788a7353
Viktor Naidenovich <nav2373@mail.ru>
2022-11-07 19:14:19 +0300

    path changed

--- plots/plot_f_phase_subcarrier.py
+++ plots/plot_f_phase_subcarrier.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from utils import calib
 
-[csi, data] = processing.read.extractCSI('/Users/viktor/dev/csiresearch/datasets/data1.dat')
+[csi, data] = processing.read.extractCSI('../datasets/data1.dat')
 s_index = [-56, -54, -52, -50, -48, -46, -44, -42, -40, -38, -36, -34, -32, -30, -28, -26, -24, -22, -20, -18, -16, -14, -12, -10, -8, -6, -4, -2, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56]
 phase = np.unwrap(np.angle(csi), axis=1)
 phase = calib(phase, s_index)
