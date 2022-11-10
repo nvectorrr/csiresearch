f237a9eac27e642e2ca50571ac2821b73ef4c740
Naidenovich Viktor <nav2373@mail.ru>
2022-11-10 16:41:28 +0300

    refactor

--- plots/deprecated/plot.py
+++ plots/deprecated/plot.py
@@ -2,7 +2,7 @@ import csiread
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = csiread.Atheros("../datasets/data2.dat", nrxnum=2, ntxnum=5, tones=56, if_report=False)
+data = csiread.Atheros("../../datasets/data2.dat", nrxnum=2, ntxnum=5, tones=56, if_report=False)
 data.read(endian='big')
 payload_len = np.bincount(data.payload_len).argmax()
 csi = data.csi[(data.payload_len == payload_len) & (data.nc == 2)][:, :, :2, :2]
