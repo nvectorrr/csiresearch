df315690d285a3d0e09ea47ab6b4ed5cfe30e415
Naidenovich Viktor <nav2373@mail.ru>
2022-11-10 16:39:15 +0300

    phase fixed

--- plots/plot_f_power_subcarrier.py
+++ plots/plot_f_power_subcarrier.py
@@ -1,13 +1,14 @@
 import processing.process
 import matplotlib.pyplot as plt
 
-[csi, data] = processing.read.extractCSI('../datasets/data1.dat')
+[csi, data] = processing.read.extractCSI('../datasets/data2.dat')
 csi = processing.process.extractAm(csi)
 csi = processing.process.reshape224x1(csi)
 
 plt.figure()
-plt.plot(range(224), csi[1], linewidth=0.3, label='power')
-plt.legend()
+for i in range(1, 224):
+    plt.plot(range(224), csi[i], linewidth=0.3, label='power')
+#plt.legend()
 
 plt.title('csi-power')
 plt.xlabel('subcarrier index')
