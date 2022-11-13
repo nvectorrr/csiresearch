86f603df96a61ed781c0afbdfe39b78b557cfba5
Naidenovich Viktor <nav2373@mail.ru>
2022-11-13 16:52:46 +0300

    refactor

--- ml/scikit/dataframe.py
+++ ml/scikit/dataframe.py
@@ -5,7 +5,7 @@ import pandas as pd
 def load_dataframe():
     train = load_csi('../../datasets/air-or-not/air_train_2.dat', 'train', 'air')
     test = load_csi('../../datasets/air-or-not/pc_case_test.dat', 'test', 'case')
-    test2 = load_csi('../../datasets/air-or-not/air_train.dat', 'test', 'air')
+    test2 = load_csi('../../datasets/air-or-not/air_test.dat', 'test', 'air')
 
     test = pd.concat([test, test2], axis=0)
 
@@ -23,12 +23,18 @@ def load_dataframe():
 
 
 def load_csi(path, type, category):
+    dataframe = get_dataframe(path)
+    dataframe['type'] = type
+    dataframe['category'] = category
+
+    return dataframe
+
+
+def get_dataframe(path):
     [csi, data] = processing.read.extractCSI(path)  # path to test file
     csi = processing.process.extractAm(csi)
     csi = processing.process.reshape224x1(csi)
 
     dataframe = pd.DataFrame(csi)
-    dataframe['type'] = type
-    dataframe['category'] = category
 
     return dataframe
