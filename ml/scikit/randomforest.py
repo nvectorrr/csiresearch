95ccdf7d38bc01887e687ee5e6ed6bafb6bafce1
Viktor Naidenovich <viktor.naidenovich@netcracker.com>
2023-02-07 20:41:24 +0300

    small refactor 2

--- ml/scikit/randomforest.py
+++ ml/scikit/randomforest.py
@@ -11,13 +11,17 @@ np.random.seed(90)
 start_time_reduced = time.time()
 x_train_reduced, y_train_reduced, x_test_reduced, y_test_reduced = load_dataframe_reduced()
 rfc_reduced = RandomForestClassifier(max_depth=20).fit(x_train_reduced, y_train_reduced)
-
-print("Accuracy reduced: " + str(rfc_reduced.score(x_test_reduced, y_test_reduced) * 100))
-print("Reduced runtime: %s seconds" % (time.time() - start_time_reduced))
+end_time_reduced = time.time() - start_time_reduced
 
 start_time_raw = time.time()
 x_train_raw, y_train_raw, x_test_raw, y_test_raw = load_dataframe_raw()
 rfc_raw = RandomForestClassifier(max_depth=20).fit(x_train_raw, y_train_raw)
+end_time_raw = time.time() - start_time_raw
 
+print('\n------------- REPORT -------------')
+print("Accuracy reduced: " + str(rfc_reduced.score(x_test_reduced, y_test_reduced) * 100))
+print("Reduced runtime: %s seconds" % (end_time_reduced))
+print()
 print("Accuracy raw: " + str(rfc_raw.score(x_test_raw, y_test_raw) * 100))
-print("Raw runtime: %s seconds" % (time.time() - start_time_raw))
+print("Raw runtime: %s seconds" % (end_time_raw))
+print('----------------------------------\n')
