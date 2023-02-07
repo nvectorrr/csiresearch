9f566a63bf2b5e47ad8f93c69392588ba0bafe8e
Viktor Naidenovich <viktor.naidenovich@netcracker.com>
2023-02-07 15:04:15 +0300

    good accuracy

--- ml/scikit/dataframe.py
+++ ml/scikit/dataframe.py
@@ -9,10 +9,10 @@ def load_dataframe_reduced():
     #test = load_csi('../../datasets/air-or-not/fourth/40MHz/metal_test.dat', 'test', 'metal')
     #test2 = load_csi('../../datasets/air-or-not/fourth/40MHz/air_test.dat', 'test', 'air')
 
-    train = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle.dat', 'train', 'bottle')
-    train2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air.dat', 'train', 'air')
-    test = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle_test.dat', 'test', 'bottle')
-    test2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air_test.dat', 'test', 'air')
+    train = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_train.dat', 'train', 'metal')
+    train2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_train.dat', 'train', 'air')
+    test = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_test.dat', 'test', 'metal')
+    test2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_test.dat', 'test', 'air')
 
     train = pd.concat([train, train2], axis=0)
     test = pd.concat([test, test2], axis=0)
@@ -21,9 +21,9 @@ def load_dataframe_reduced():
     test = test.sample(frac=1).reset_index(drop=True)
     labels = train['category']
 
-    fs = FeatureSelector(data = train, labels = labels)
-    fs.identify_zero_importance(task = 'classification', eval_metric = 'auc',
-                            n_iterations = 5, early_stopping = True)
+    fs = FeatureSelector(data=train, labels=labels)
+    fs.identify_zero_importance(task='classification', eval_metric='auc',
+                            n_iterations=10, early_stopping = True)
 
     one_hot_features = fs.one_hot_features
     base_features = fs.base_features
@@ -31,9 +31,9 @@ def load_dataframe_reduced():
     print('There are %d original features' % len(base_features))
     print('There are %d one-hot features' % len(one_hot_features))
 
-    fs.identify_low_importance(cumulative_importance=0.99)
+    fs.identify_low_importance(cumulative_importance=0.95)
 
-    train = fs.remove(methods = {'zero_importance', 'low_importance'})
+    train = fs.remove(methods = {'low_importance'})
     removed = fs.check_removal()
     test_drop_filter = test.filter(removed)
     test.drop(test_drop_filter, inplace=True, axis=1)
@@ -46,10 +46,10 @@ def load_dataframe_reduced():
     return x_train, y_train, x_test, y_test
 
 def load_dataframe_raw():
-    train = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle.dat', 'train', 'bottle')
-    train2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air.dat', 'train', 'air')
-    test = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle_test.dat', 'test', 'bottle')
-    test2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air_test.dat', 'test', 'air')
+    train = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_train.dat', 'train', 'metal')
+    train2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_train.dat', 'train', 'air')
+    test = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_test.dat', 'test', 'metal')
+    test2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_test.dat', 'test', 'air')
 
     train = pd.concat([train, train2], axis=0)
     test = pd.concat([test, test2], axis=0)
