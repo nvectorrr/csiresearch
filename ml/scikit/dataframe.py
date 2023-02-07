c3bdd673e903103f0c69d807f6441c565342b1e0
Viktor Naidenovich <viktor.naidenovich@netcracker.com>
2023-02-07 14:59:07 +0300

    regression attempt

--- ml/scikit/dataframe.py
+++ ml/scikit/dataframe.py
@@ -9,21 +9,27 @@ def load_dataframe_reduced():
     #test = load_csi('../../datasets/air-or-not/fourth/40MHz/metal_test.dat', 'test', 'metal')
     #test2 = load_csi('../../datasets/air-or-not/fourth/40MHz/air_test.dat', 'test', 'air')
 
-    train = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle.dat', 'train', 'bottle')
-    train2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air.dat', 'train', 'air')
-    test = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle_test.dat', 'test', 'bottle')
-    test2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air_test.dat', 'test', 'air')
+    train = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_train.dat', 'train', 'metal')
+    train2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_train.dat', 'train', 'air')
+    test = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_test.dat', 'test', 'metal')
+    test2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_test.dat', 'test', 'air')
 
     train = pd.concat([train, train2], axis=0)
     test = pd.concat([test, test2], axis=0)
 
     train = train.sample(frac=1).reset_index(drop=True)
     test = test.sample(frac=1).reset_index(drop=True)
+    train['category'] = train['category'].replace(['air'], 0)
+    train['category'] = train['category'].replace(['metal'], 1)
+    test['category'] = test['category'].replace(['air'], 0)
+    test['category'] = test['category'].replace(['metal'], 1)
     labels = train['category']
 
-    fs = FeatureSelector(data = train, labels = labels)
-    fs.identify_zero_importance(task = 'classification', eval_metric = 'auc',
-                            n_iterations = 5, early_stopping = True)
+    fs = FeatureSelector(data=train, labels=labels)
+    fs.identify_zero_importance(task='regression', eval_metric='l2',
+                            n_iterations=10, early_stopping=True)
+
+    fs.identify_low_importance(cumulative_importance=0.95)
 
     one_hot_features = fs.one_hot_features
     base_features = fs.base_features
@@ -31,14 +37,12 @@ def load_dataframe_reduced():
     print('There are %d original features' % len(base_features))
     print('There are %d one-hot features' % len(one_hot_features))
 
-    fs.identify_low_importance(cumulative_importance=0.99)
-
     train = fs.remove(methods = {'zero_importance', 'low_importance'})
     removed = fs.check_removal()
     test_drop_filter = test.filter(removed)
     test.drop(test_drop_filter, inplace=True, axis=1)
 
-    x_train = train.drop(['type', 'category', 'category_air'], axis=1)
+    x_train = train.drop(['type', 'category'], axis=1)
     y_train = train['category']
     x_test = test.drop(['type', 'category'], axis=1)
     y_test = test['category']
@@ -46,10 +50,10 @@ def load_dataframe_reduced():
     return x_train, y_train, x_test, y_test
 
 def load_dataframe_raw():
-    train = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle.dat', 'train', 'bottle')
-    train2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air.dat', 'train', 'air')
-    test = load_csi('../../datasets/air-or-not/fourth/20MHz/bottle_test.dat', 'test', 'bottle')
-    test2 = load_csi('../../datasets/air-or-not/fourth/20MHz/air_test.dat', 'test', 'air')
+    train = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_train.dat', 'train', 'metal')
+    train2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_train.dat', 'train', 'air')
+    test = load_csi('../../datasets/air-or-not/fifth/20MHz/metal_test.dat', 'test', 'metal')
+    test2 = load_csi('../../datasets/air-or-not/fifth/20MHz/air_test.dat', 'test', 'air')
 
     train = pd.concat([train, train2], axis=0)
     test = pd.concat([test, test2], axis=0)
--- ml/scikit/randomforest.py
+++ ml/scikit/randomforest.py
@@ -1,12 +1,17 @@
 from sklearn.ensemble import RandomForestClassifier
 from dataframe import *
+import time
 
+start_time = time.time()
 x_train, y_train, x_test, y_test = load_dataframe_reduced()
 
 rfc_reduced = RandomForestClassifier(max_depth=20).fit(x_train, y_train)
 print("Accuracy reduced: " + str(rfc_reduced.score(x_test, y_test) * 100))
+print("Reduced runtime: %s seconds" % (time.time() - start_time))
 
+start_time = time.time()
 x_train, y_train, x_test, y_test = load_dataframe_raw()
 
 rfc_raw = RandomForestClassifier(max_depth=20).fit(x_train, y_train)
 print("Accuracy raw: " + str(rfc_raw.score(x_test, y_test) * 100))
+print("Raw runtime: %s seconds" % (time.time() - start_time))
